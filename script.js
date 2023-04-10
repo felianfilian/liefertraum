7025c8a09055205decb35e606527db4db6ee8516
Dennis Schmalstieg <darianbellentor@outlook.de>
2023-04-10 19:42:51 +0200

    sum logic

--- script.js
+++ script.js
@@ -1,4 +1,42 @@
-let items = [];
-let prices = [];
+let items = ["Cheeseburger"];
+let prices = [5.6];
 
-function addItem() {}
+let orderPrices = [];
+
+let orderItems = document.getElementById("order-items");
+let totalSum = document.getElementById("total-sum");
+
+orderItems.innerHTML = `
+<div style="text-align: center;">
+<img src="img/bag.png">
+<br>
+Wähle leckere Gerichte aus der Karte
+<br>
+und bestelle Dein Menu
+</div>
+`;
+
+function addItem() {
+  orderPrices.push(prices[0]);
+
+  orderItems.innerHTML += `
+  <div class="order-item">
+    <p>1x</p>
+    <p>${items[0]}</p>
+    <p>${prices[0].toFixed(2)} €</p>
+  </div>
+  `;
+
+  showTotalSum();
+}
+
+function showTotalSum() {
+  let sum = 0;
+  for (let item of orderPrices) {
+    sum += item;
+  }
+
+  totalSum.innerHTML = `
+  <b>Gesamt ${sum.toFixed(2)} €</b>
+  `;
+}
