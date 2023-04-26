ddf269983c30be98d692cdfcd203e616cd75d532
felianfilian <darianbellentor@outlook.de>
2023-04-26 20:11:11 +0200

    amount change button test 01

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
-let items = ["Cheeseburger"];
-let prices = [5.6];
+let orderPrice = [];
+let deliveryPrice = 2.0;
 
 let orderNames = [];
-let orderPrices = [];
+let orderSum = [];
 let orderAmounts = [];
 
 let orderItems = document.getElementById("order-items");
 let subSum = document.getElementById("sub-sum");
 let deliverySum = document.getElementById("delivery-sum");
 let totalSum = document.getElementById("total-sum");
+
+let msubSum = document.getElementById("m-sub-sum");
+let mdeliverySum = document.getElementById("m-delivery-sum");
+let mtotalSum = document.getElementById("m-total-sum");
 
 orderItems.innerHTML = `
 <div style="text-align: center;">
@@ -20,30 +24,35 @@ und bestelle Dein Menu
 </div>
 `;
 
-function addItem(orderName, orderPrice) {
+function addItem(orderName, newPrice) {
   if (orderNames.includes(orderName)) {
     let orderNameIndex = orderNames.indexOf(orderName);
     orderAmounts[orderNameIndex]++;
-    orderPrices[orderNameIndex] = orderPrice * orderAmounts[orderNameIndex];
+    orderSum[orderNameIndex] =
+      orderPrice[orderNameIndex] * orderAmounts[orderNameIndex];
   } else {
     orderAmounts.push(1);
     orderNames.push(orderName);
-    orderPrices.push(orderPrice);
+    orderPrice.push(newPrice);
+    orderSum.push(newPrice);
   }
 
-  showShoppingCart(orderNames, orderPrices, orderAmounts);
+  showShoppingCart(orderNames, orderSum, orderAmounts);
   showTotalSum();
 }
 
-function showShoppingCart(orderNames, orderPrices, orderAmounts) {
+function showShoppingCart(orderNames, orderSum, orderAmounts) {
   orderItems.innerHTML = "";
   for (let i = 0; i < orderNames.length; i++) {
     orderItems.innerHTML += `
   <div class="order-item">
     <p>${orderAmounts[i]}x</p>
     <p>${orderNames[i]}</p>
-    <p>${orderPrices[i].toFixed(2).replace(".", ",")} €</p>
-    <img class="btn-bucket" src="img/bucket-red.png" alt="">
+    <p>${orderSum[i].toFixed(2).replace(".", ",")} €</p>
+  </div>
+  <div class="amount-buttons">
+    <a class="btn-amount" onclick="addItem(${orderNames[i]})">-</a>
+    <a class="btn-amount" onclick="amountUp(${i})">+</a>
   </div>
   `;
   }
@@ -51,11 +60,40 @@ function showShoppingCart(orderNames, orderPrices, orderAmounts) {
 
 function showTotalSum() {
   let sum = 0;
-  for (let item of orderPrices) {
+  for (let item of orderSum) {
     sum += item;
   }
+  subSum.innerHTML = `
+  <b>Zwischensumme ${sum.toFixed(2).replace(".", ",")} €</b>
+  `;
+  deliverySum.innerHTML = `
+  <b>Lieferkosten ${deliveryPrice.toFixed(2)} €</b>
+  `;
+  msubSum.innerHTML = `
+  <b>Zwischensumme ${sum.toFixed(2).replace(".", ",")} €</b>
+  `;
+  mdeliverySum.innerHTML = `
+  <b>Lieferkosten ${deliveryPrice.toFixed(2)} €</b>
+  `;
+
+  sum += deliveryPrice;
 
   totalSum.innerHTML = `
   <b>Gesamt ${sum.toFixed(2).replace(".", ",")} €</b>
   `;
+  mtotalSum.innerHTML = `
+  <b>Gesamt ${sum.toFixed(2).replace(".", ",")} €</b>
+  `;
+}
+
+function amountDown(index) {
+  orderAmounts[index]--;
+  showShoppingCart(orderNames, orderSum, orderAmounts);
+  showTotalSum();
+}
+
+function amountUp(index) {
+  orderAmounts[index]++;
+  showShoppingCart(orderNames, orderSum, orderAmounts);
+  showTotalSum();
 }
